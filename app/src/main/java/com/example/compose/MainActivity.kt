2abf1088a3d0c6bee7b8c297c3398a941082fb02
Gaurav ROy <40909110+gauravroy1995@users.noreply.github.com>
2023-10-17 12:33:44 +0530

    added next prev , pending is the swipe gesture

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -6,38 +6,66 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.compose.ui.theme.ComposeTheme
-import java.text.DecimalFormat
-import java.util.Currency
-import java.util.Locale
-import kotlin.math.roundToInt
 
 
-const val nameI = "India"
+//const val nameI = "https://images.unsplash.com/photo-1607604276583-eef5d076aa5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+//"https://images.unsplash.com/photo-1560972550-aba3456b5564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGFuaW1lfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+//"https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+
+data class MyItem(
+    val title: String,
+    val description: String,
+    val imageUrl: String
+)
+
+
+val myMutableObjectList: MutableList<MyItem> = mutableListOf(
+    MyItem(
+        "Title 1",
+        "Description 1",
+        "https://images.unsplash.com/photo-1607604276583-eef5d076aa5f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 2",
+        "Description 2",
+        "https://images.unsplash.com/photo-1560972550-aba3456b5564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGFuaW1lfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 3",
+        "Description 3",
+        "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+    )
+)
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +77,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    RenderGreeting(nameI)
+                    RenderGreeting()
                 }
             }
         }
@@ -57,100 +85,85 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun RenderTextInput(billString:String, onValueChangeNew:(String)->Unit, modifier: Modifier = Modifier) {
-    TextField(
-        value = billString,
-        onValueChange = onValueChangeNew ,
-        singleLine = true,
-        label = { Text("Total Amount") },
-        leadingIcon = { Icon(painter = painterResource(id = R.drawable.money), contentDescription = null)},
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        modifier = modifier
-    )
+fun RenderThings(currData: MyItem) {
+    val image = currData.imageUrl;
+    val title = currData.title
+    val description = currData.description
+    Box(
+        modifier = Modifier
+            .border(1.dp, Color.Black)
+            .background(Color.White)
+    ) {
+        AsyncImage(
+            model = image, contentDescription = null,
+            modifier = Modifier
+                .height(200.dp)
+                .width(200.dp)
+                .padding(10.dp),
+            contentScale = ContentScale.FillBounds,
+
+            )
+    }
+    Text(text = title)
+    Text(text = description)
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RenderTipPercent(billString:String, onValueChangeNew:(String)->Unit, modifier: Modifier = Modifier.padding(top = 20.dp)) {
-    TextField(
-        value = billString,
-        onValueChange = onValueChangeNew ,
-        singleLine = true,
-        leadingIcon = { Icon(painter = painterResource(id = R.drawable.tip), contentDescription = null)},
-        label = { Text("Tip Percent") },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-        modifier = modifier
-    )
-}
 
 @Composable
-fun RenderSwitch(round:Boolean, onValueChangeNew:(Boolean)->Unit, modifier: Modifier = Modifier
-    .padding(top = 10.dp, start = 60.dp, end = 60.dp)
-    .fillMaxWidth()){
-    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.SpaceBetween, modifier =modifier ){
-        Text("Round off")
-        Switch(checked = round, onCheckedChange =onValueChangeNew)
+fun RenderButtons(currIndex: Int = 0, onNextPress: () -> Unit, onPrevPress: () -> Unit) {
+    Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+        if (currIndex > 0) {
+            Button(onClick = { onPrevPress() }) {
+                Text(text = "Previous")
+            }
+        }
+
+        if (currIndex < myMutableObjectList.size - 1) {
+            Button(onClick = onNextPress) {
+                Text(text = "Next")
+            }
+        }
     }
 }
 
+
 @Composable
-fun checkTip(amount: String, tip: Double = 0.0, isRound: Boolean = false): String {
-    var total = amount.toDoubleOrNull() ?: 0.0
-    var tipString = total * tip;
-    if (isRound) {
-        tipString = tipString.roundToInt().toDouble()
+fun RenderGreeting() {
+
+    var (currIndex, setCurrIndex) = remember { mutableStateOf(0) }
+    var swipePerformed by remember { mutableStateOf(false) }
+
+    var currData: MyItem = myMutableObjectList[currIndex]
+
+
+    fun onNextPress() {
+        if (currIndex < myMutableObjectList.size - 1) {
+            setCurrIndex(currIndex + 1)
+
+        }
     }
 
-    val decimalFormat = DecimalFormat("#.##")
-
-    return decimalFormat.format(tipString)
-}
-
-@Composable
-fun calculateTipPercent(amount: String): Double {
-    val tip =  amount.toDoubleOrNull() ?: 0.0;
-    return tip/100;
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RenderTipTextBox(modifier: Modifier) {
-
-
-    var (billString,setBillString) =  remember { mutableStateOf("") }
-    var (tipPercent,setTipPercent) =  remember { mutableStateOf("") }
-    var (round,setRound) =  remember { mutableStateOf(false) }
-
-    val tipPercentDouble = calculateTipPercent(tipPercent)
-    val tip = checkTip(billString,tipPercentDouble,round)
-
-
-    val currencyCode = Currency.getInstance(Locale.getDefault()).currencyCode
-    val currencySymbol = Currency.getInstance(currencyCode).getSymbol(Locale.getDefault())
-
-    
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()) {
-        RenderTextInput(billString, { setBillString(it) })
-        RenderTipPercent(tipPercent, { setTipPercent(it) })
-        RenderSwitch(round, { setRound(it) })
-
-        Text("Your tip $currencySymbol$tip", modifier = Modifier,fontSize = 32.sp)
-
+    fun onPrevPress() {
+        if (currIndex > 0) {
+            setCurrIndex(currIndex - 1)
+        }
     }
-   
 
-}
-
-
+    val offsetX = remember { mutableStateOf(0f) }
+    var width by remember { mutableStateOf(0f) }
 
 
-@Composable
-fun RenderGreeting(name: String, modifier: Modifier = Modifier) {
-
-    RenderTipTextBox(Modifier.padding(5.dp))
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        RenderThings(currData)
+        RenderButtons(currIndex, {
+            onNextPress()
+        }, { onPrevPress() })
+    }
 
 }
 
@@ -158,6 +171,6 @@ fun RenderGreeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ComposeTheme {
-        RenderGreeting(nameI)
+        RenderGreeting()
     }
 }
