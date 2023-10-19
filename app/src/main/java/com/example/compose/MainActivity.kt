9d331ed4033e04c32848f9bad6781e6e2d065714
Gaurav ROy <40909110+gauravroy1995@users.noreply.github.com>
2023-10-19 22:27:36 +0530

    add lazy column

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -4,33 +4,27 @@ package com.example.compose
 
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,6 +53,36 @@ val myMutableObjectList: MutableList<MyItem> = mutableListOf(
         "Title 2",
         "Description 2",
         "https://images.unsplash.com/photo-1560972550-aba3456b5564?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGFuaW1lfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 3",
+        "Description 3",
+        "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 3",
+        "Description 3",
+        "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 3",
+        "Description 3",
+        "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 3",
+        "Description 3",
+        "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 3",
+        "Description 3",
+        "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
+    ),
+    MyItem(
+        "Title 3",
+        "Description 3",
+        "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YW5pbWV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60"
     ),
     MyItem(
         "Title 3",
@@ -112,75 +136,38 @@ fun RenderThings(currData: MyItem) {
 
 
 @Composable
-fun RenderButtons(currIndex: Int = 0, onNextPress: () -> Unit, onPrevPress: () -> Unit) {
-    Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
-        if (currIndex > 0) {
-            Button(onClick = { onPrevPress() }) {
-                Text(text = "Previous")
-            }
-        }
+fun RenderList(cardData:MyItem){
 
-        if (currIndex < myMutableObjectList.size - 1) {
-            Button(onClick = onNextPress) {
-                Text(text = "Next")
-            }
+    val image = cardData.imageUrl
+    val title = cardData.title
+
+    Card(modifier = Modifier.padding(vertical = 20.dp)){
+        Column() {
+            AsyncImage(model = image, contentDescription = title, modifier = Modifier
+                .height(200.dp)
+                .width(200.dp), contentScale = ContentScale.FillBounds)
+            Text(text = title)
         }
     }
+
 }
 
 
 @Composable
 fun RenderGreeting() {
 
-    var (currIndex, setCurrIndex) = remember { mutableStateOf(0) }
 
-
-    var currData: MyItem = myMutableObjectList[currIndex]
-
-
-    fun onNextPress() {
-        if (currIndex < myMutableObjectList.size - 1) {
-            Log.d("Gaurav drag press cuteee","press ${currIndex}")
-            setCurrIndex(currIndex + 1)
-
-        }
-    }
-
-    fun onPrevPress() {
-        Log.d("Gaurav drag press","press")
-        if (currIndex > 0) {
-            Log.d("Gaurav drag press nowww","press")
-            setCurrIndex(currIndex - 1)
+    LazyColumn(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally , modifier = Modifier.fillMaxSize()){
+        items(myMutableObjectList.size){
+            index ->
+             RenderList(cardData = myMutableObjectList[index])        }
         }
     }
 
 
 
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize().pointerInput(Unit) {
-            var totalDrag = 0f
-            detectHorizontalDragGestures(onHorizontalDrag = {change, dragAmount -> totalDrag+= dragAmount},
-                onDragStart = {}, onDragEnd = {
-                    if(totalDrag > 0) {
-                        onNextPress()
-                    } else {
-                        onPrevPress()
-                    }
-//                    totalDrag = 0f
-                    Log.d("Gaurav drag", "onDragEnd: $totalDrag")
 
-                }, onDragCancel = {})
-        }
-    ) {
-        RenderThings(currData)
-        RenderButtons(currIndex, {
-            onNextPress()
-        }, { onPrevPress() })
-    }
 
-}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
---
+++ app/src/main/java/com/example/compose/model/ImageDataClass.kt
@@ -0,0 +1,7 @@
+package com.example.compose.model
+
+data class ImageDataClass(
+    val title: String,
+    val description: String,
+    val imageUrl: String
+)
