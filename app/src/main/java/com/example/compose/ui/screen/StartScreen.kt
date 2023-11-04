082722cdad176f919aee00ee60030edb814a2cd3
Gaurav ROy <40909110+gauravroy1995@users.noreply.github.com>
2023-11-04 11:07:53 +0530

    removed unncessary code

--- app/src/main/java/com/example/compose/ui/screen/StartScreen.kt
+++ app/src/main/java/com/example/compose/ui/screen/StartScreen.kt
@@ -1,21 +1,17 @@
 package com.example.compose.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun StartScreen(onClick: () -> Unit){
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()){
+
+    Column() {
+        Text(text = "Start Screen")
         Button(onClick = onClick) {
-            Text(text ="Start", modifier = Modifier.padding(horizontal = 25.dp))
+            Text(text = "Start")
         }
     }
 }
